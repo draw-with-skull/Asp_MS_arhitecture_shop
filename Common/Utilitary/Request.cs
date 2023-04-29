b829099ecf3723a279c0a2cb28205cd04aaad509
Razvi <drawwithskull@gmail.com>
2023-04-29 23:59:54 +0300

    Request error when container is down

--- Common/Utilitary/Request.cs
+++ Common/Utilitary/Request.cs
@@ -12,77 +12,136 @@ namespace Common.Utilitary
         #region Product
         public async Task<List<Product>> GetAllItems()
         {
-            return await client.GetFromJsonAsync<List<Product>>(Endpoints.STORAGE.GET_ITEMS)??new();
-        }
+            try
+            {
+				return await client.GetFromJsonAsync<List<Product>>(Endpoints.STORAGE.GET_ITEMS) ?? new();
+			}
+			catch (Exception) { return new(); }//silence ignore,storage ms is not working
+		}
 
         public async Task<Product> GetItemByID(string id) {
-            return await client.GetFromJsonAsync<Product>(Endpoints.STORAGE.GET_ITEM+$"/{id}")??new();
-        }
+			try
+			{
+				return await client.GetFromJsonAsync<Product>(Endpoints.STORAGE.GET_ITEM+$"/{id}")??new();
+		    }
+			catch (Exception) { return new(); }//silence ignore,storage ms is not working
+		}
 
         public async Task<List<Product>> GetDiscounted()
         {
-            return await client.GetFromJsonAsync<List<Product>>(Endpoints.STORAGE.GET_DISCOUNTED_ITEMS)??new();
+			try
+			{
+				return await client.GetFromJsonAsync<List<Product>>(Endpoints.STORAGE.GET_DISCOUNTED_ITEMS)??new();
+            }
+            catch (Exception){return new();}//silence ignore,storage ms is not working
         }
 
         public async Task<List<Product>> GetItemsWithTag(string tag)
         {
-            return await client.GetFromJsonAsync<List<Product>>(Endpoints.STORAGE.GET_ITEM_BY_TAG + $"/{tag}")??new();    
+            try 
+            { 
+                return await client.GetFromJsonAsync<List<Product>>(Endpoints.STORAGE.GET_ITEM_BY_TAG + $"/{tag}")??new();
+		    }
+            catch (Exception){return new ();}//silence ignore,storage ms is not working
         }
-
-        public async Task RemoveItemByID(string id)
+	    public async Task RemoveItemByID(string id)
         {
-            await client.DeleteAsync(Endpoints.STORAGE.DELETE_ITEM + $"/{id}");
+			try
+			{
+				await client.DeleteAsync(Endpoints.STORAGE.DELETE_ITEM + $"/{id}");
+			}
+			catch (Exception) {}//silence ignore,storage ms is not working
         }
 
         public async Task RemoveItemFromUserCart(string userId,string productId)
         {
-            await client.DeleteAsync(Endpoints.STORAGE.DELETE_ITEM_FROM_USER_CART + $"/{userId}/{productId}");
+			try
+			{
+				await client.DeleteAsync(Endpoints.STORAGE.DELETE_ITEM_FROM_USER_CART + $"/{userId}/{productId}");
+			}
+			catch (Exception) { }//silence ignore,storage ms is not working
         }
 
         public async Task SaveItem(Product product)
         {
-           await client.PostAsJsonAsync(Endpoints.STORAGE.POST_ITEM, product);
+			try
+			{
+				await client.PostAsJsonAsync(Endpoints.STORAGE.POST_ITEM, product);
+			}
+			catch (Exception) {}//silence ignore,storage ms is not working
         }
         
         
         public async Task UpdateItem(Product product)
         {
-            await client.PostAsJsonAsync(Endpoints.STORAGE.UPDATE_ITEM, product);
+			try
+			{
+				await client.PostAsJsonAsync(Endpoints.STORAGE.UPDATE_ITEM, product);
+			}
+			catch (Exception) {}//silence ignore,storage ms is not working
         }
         #endregion
         #region User
         public async Task SaveItem(User user)
         {
-            await client.PostAsJsonAsync(Endpoints.STORAGE.POST_USER, user);
+			try
+			{
+				await client.PostAsJsonAsync(Endpoints.STORAGE.POST_USER, user);
+			}
+			catch (Exception) {}//silence ignore,storage ms is not working
         }
         public async Task<User> GetUserData(string username,string password)
         {
-            return await client.GetFromJsonAsync<User>(Endpoints.STORAGE.GET_USER+ $"/{username}/{password}")??new();
+			try
+			{
+				return await client.GetFromJsonAsync<User>(Endpoints.STORAGE.GET_USER+ $"/{username}/{password}")??new();
+			}
+			catch (Exception) { return new(); }//silence ignore,storage ms is not working
         }
 
         public async Task<List<Product>> GetUserProductList(string username)
         {
-            return await client.GetFromJsonAsync<List<Product>>(Endpoints.STORAGE.GET_USER_SHOPPING_CART+$"/{username}")??new();
+			try
+			{
+				return await client.GetFromJsonAsync<List<Product>>(Endpoints.STORAGE.GET_USER_SHOPPING_CART+$"/{username}")??new();
+			}
+			catch (Exception) { return new(); }//silence ignore,storage ms is not working
         }
         public async Task UpdateUserShoppingCart(User user)
         {
-            await client.PostAsJsonAsync(Endpoints.STORAGE.UPDATE_CART_LIST,user);
+			try
+			{
+			await client.PostAsJsonAsync(Endpoints.STORAGE.UPDATE_CART_LIST,user);
+			}
+			catch (Exception) {}//silence ignore,storage ms is not working
         }
 		#endregion
 
 		#region Order
 		public async Task SaveItem(Order order)
 		{
+			try
+			{
 			await client.PostAsJsonAsync(Endpoints.STORAGE.POST_ORDER, order);
+			}
+			catch (Exception) {}//silence ignore,storage ms is not working
 		}
 		public async Task<List<Order>> GetUnfinishedOrders()
         {
-            return await client.GetFromJsonAsync<List<Order>>(Endpoints.STORAGE.GET_UNFINISHED_ORDERS)??new();
+			try
+			{
+			return await client.GetFromJsonAsync<List<Order>>(Endpoints.STORAGE.GET_UNFINISHED_ORDERS)??new();
+			}
+			catch (Exception) { return new(); }//silence ignore,storage ms is not working
         }
 
         public async Task UpdateOrder(Order order)
         {
-            await client.PostAsJsonAsync(Endpoints.STORAGE.UPDATE_ORDER, order);
+			try
+			{
+			await client.PostAsJsonAsync(Endpoints.STORAGE.UPDATE_ORDER, order);
+			}
+			catch (Exception) {}//silence ignore,storage ms is not working
         }
         #endregion
     }
